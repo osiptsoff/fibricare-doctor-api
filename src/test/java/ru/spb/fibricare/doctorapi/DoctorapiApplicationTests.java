89a5394bc8c9deb96dece8735194506144270eb5
osiptsoff <osipczovnikita@gmail.com>
2024-04-25 02:39:08 +0300

    init

---
+++ src/test/java/ru/spb/fibricare/doctorapi/DoctorapiApplicationTests.java
@@ -0,0 +1,9 @@
+package ru.spb.fibricare.doctorapi;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan
+class DoctorapiApplicationTests {
+}
